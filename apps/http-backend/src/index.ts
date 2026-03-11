ff44879b3be72f700232c49d17b7e727a1bc9414
Deepak Aggarwal <deepakagg1206@gmail.com>
2026-03-12 00:37:56 +0530

    Backend port configured

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -267,4 +267,6 @@ app.get("/check-auth",(req,res) => {
   }
 })
 
-app.listen(3001);
+app.listen(Number(process.env.PORT) || 3001, "0.0.0.0", () => {
+  console.log(`Server is running on port ${process.env.PORT || 3001}`);
+});
